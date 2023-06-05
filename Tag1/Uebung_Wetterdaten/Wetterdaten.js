01fafcdff3ff60fd8a39b9ecb26a7a0d94f07951
Cast0rM <113532532+Cast0rM@users.noreply.github.com>
2023-06-05 18:11:44 +0200

    korrektur Uebung Wetterdaten

--- Tag1/Uebung_Wetterdaten/Wetterdaten.js
+++ Tag1/Uebung_Wetterdaten/Wetterdaten.js
@@ -1,3 +1,10 @@
+fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')
+ .then((response)=> response.json())
+ .then((data)=>{
+    console.log(`Die Daten sind: ${data.hourly.time}`)
+ });
+
+
 // var https = require('https')
 // let url = "https://the-trivia-api.com/v2/questions"
 
@@ -6,9 +13,3 @@
 //     response.on('data', console.log)
 //     response.on('error', console.error)
 // })
-
-fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')
- .then((response)=> response.json())
- .then((data)=>{
-    console.log(`Die Daten sind: ${data.hourly.time}`)
- })
