a6d230ab8cd97c408da5cf975c131073a2ba7392
Cast0rM <113532532+Cast0rM@users.noreply.github.com>
2023-06-05 15:40:14 +0200

    Readme des Repositories hinzugefügt

--- Readme.md
+++ Readme.md
@@ -1,2 +1,2 @@
 # Castor M. Fernández Lado | m295
-Auf [LearnYouNode_Aufgaben](./learnyounode_Aufgaben/) finden wir die gelöste Aufgaben, die wir machen mussten.  
+Alles was wir am [Tag 1](./Tag1/) gemacht haben, ist hier gespeichert worden.
---
+++ Tag1/Aufgabe_Promises/promises.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+function leseDateiInhalt(filepath){
+    return new Promise(function(resolve, reject){
+        fs.readFile(filepath, (err, data)=>{
+            if(!err)
+            {
+                resolve(data);
+            }
+            reject(err)
+        });
+    });
+};
+leseDateiInhalt(process.argv[2])
+.then(inhalt => {
+    console.log('Die Länge der Dateiinhalts beträgt: ' + inhalt.length)
+})
+.catch(err=> {
+    console.error('Fehler beim Lesen der Datei: ' + err)
+});
