a94aa8fb63b3220137588ee2b656a0ea3946f3f1
Cast0rM <113532532+Cast0rM@users.noreply.github.com>
2023-06-05 13:46:43 +0200

    variable resultat umbenent auf result

--- babySteps/baby-steps.js
+++ babySteps/baby-steps.js
@@ -1,6 +1,6 @@
 let resultat = 0
 
 for (let i = 2; i < process.argv.length; i++)
-  resultat += Number(process.argv[i])
+  result += Number(process.argv[i])
 
-console.log(resultat)
+console.log(result)
