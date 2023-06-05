a4b556bfefc413ceea6d0e40b9e126b203447e4a
Cast0rM <castor.mfl@gmail.com>
2023-06-05 13:30:28 +0200

    test baby-steps

--- babySteps/baby-steps.js
+++ babySteps/baby-steps.js
@@ -1,6 +1,6 @@
-var resultat = 0
+let resultat = 0
 
-for (var i = 2; i < process.argv.length; i++)
+for (let i = 2; i < process.argv.length; i++)
   resultat += Number(process.argv[i])
 
 console.log(resultat)
