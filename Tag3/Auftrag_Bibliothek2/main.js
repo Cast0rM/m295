b7178df472a4810c63a7427562f80c76b6ac20c9
Cast0rM <113532532+Cast0rM@users.noreply.github.com>
2023-06-12 15:19:29 +0200

    musterlösung Uebung Library 2

--- Tag3/Auftrag_Bibliothek2/main.js
+++ Tag3/Auftrag_Bibliothek2/main.js
@@ -1,13 +1,16 @@
 const express = require("express")
 const app = express()
 const port = 3000
-const date = new Date().toLocaleDateString("de-CH", { timeZone: "Europe/Zurich" })
+//const date = new Date().toLocaleDateString("de-CH", { timeZone: "Europe/Zurich" })
+const date = new Date().toISOString()
+app.use(express.json())
+
 let lends = [
     {
         id: 123,
         customer_id: "A1",
         isbn: 97896452321,
-        borrowed_at: date,
+        borrowed_at: date, //new Date.toISOString
         returned_at: ""
     },
     {
@@ -28,11 +31,13 @@ let lends = [
 
 app.use(express.json())
 
+// GET /lends
 app.get('/lends', (req, res) => {
     res.json(lends)
 
 })
 
+// GET /lends/id
 app.get('/lends/:id', (req, res) => {
     const id = req.params.id
     const lend = lends.find(l => l.id == id)
@@ -43,38 +48,45 @@ app.get('/lends/:id', (req, res) => {
 
 })
 
+// POST /lends
 app.post('/lends', (req, res) => {
     const newLend = req.body
+    newLend.id = lends.length + 1;
+    newLend.borrowed_at = date;
+    newLend.returned_at = null;
+    if (!isValid(newLend)) return res.sendStatus(422)
 
-    if (isValid(newLend)) {
-        lends.push(newLend)
-        res.json(newLend)
-    }
-    else {
-        res.sendStatus(422)
-    }
+    lends.push(newLend)
+    res.send(newLend)
 })
 
+// PATCH /lends/id
 app.patch('/lends/:id', (req, res) => {
     const id = req.params.id
     const lendIndex = lends.findIndex(l => l.id == id)
     const lendUpdate = req.body
 
-    if (lendIndex < 0) {
-        return res.sendStatus(404)
+    if (id != lendUpdate.id) {
+        res.sendStatus(422);
     }
-    if (!isValid(lendUpdate)) {
-        return res.sendStatus(422)
+    if (lendIndex < 0) return res.sendStatus(404)
+    if (!isValid(result, false)) {
+        res.sendStatus(422)
     }
-
-    const result = Object.assign(lends[lendIndex], lendUpdate)
-    res.status(200).json(lendUpdate)
-
+    else {
+        const result = Object.assign(lends[lendIndex], lendUpdate)
+        console.log(result)
+        lends.splice(lendIndex, 1, result)
+        res.status(200).json(result)
+    }
 })
 
 
-function isValid(lend) {
-    return lend.id != undefined && lend.id != "" &&
+function isValid(lend, checkIds = true) {
+
+    return Boolean(lends.find(l => l.id == lend.id)) == false &&
+        Boolean(lends.find(l => l.isbn == lend.isbn)) == false &&
+        lend.id != undefined && lend.id != "" &&
         lend.customer_id != undefined && lend.customer_id != "" &&
         lend.isbn != undefined && lend.isbn != "" &&
         lend.borrowed_at != undefined && lend.borrowed_at != ""
